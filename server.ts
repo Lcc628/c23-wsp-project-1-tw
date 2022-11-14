65bf2b715f2eefb64ff735ff8b00c9b062e766b2
ctpng0725 <chriswu14725@gmail.com>
2022-11-14 18:05:39 +0800

    error

--- server.ts
+++ server.ts
@@ -56,6 +56,18 @@ app.post('/login', async (req, res) => {
 })
 
 // //logout
+// app.get('/logout', (req, res) => {
+//   req.session.destroy(function(err){
+//     if(err){
+//       console.log(err);
+//       res.send("Error")
+//     } else {
+//       res.render('base',{title:"Express",logout : "Logout Successfully!!"})
+//     }
+//   })
+// })
+
+
 // app.get("/logout", logout);
 // async function logout(req: express.Request, res: express.Response) {
 //   delete req.session.user;
